b45c95686f7af879b31a688feb4af235da5865ce
Janne Toivola <jammu81@iki.fi>
2015-10-11 17:22:40 +0300

    More memtest ideas

--- src/memtest.asm
+++ src/memtest.asm
@@ -1,34 +1,44 @@
 .. Tests each byte of RAM by writing and reading the given pattern.
-.. Starts from the last address and counts down to the last byte occupied
-.. by this program.
+.. Starts from the first free byte and counts to the last byte available.
+.. Turns Q on in case of failure.
 .. Author: Janne Toivola
-0000 E1 START:  SEX 1  .. X = 1, use R1 as the address register for I/O
+0000 7A INIT:   REQ    .. Turn Q off to be sure
 0001 90         GHI 0  .. D = R0.hi (== 0)
 0002 B1         PHI 1  .. R1.hi = D
-0003 F8         LDI    .. D = 
-0004 10                ..   0x10 FIXME: END+1
-0005 A1         PLO 1  .. R1.lo = D
-0006 6C INPUT:  INP 4  .. *R1 = D = switches
-0007 E2 INIT:   SEX 2  .. X = 2, use R2 as the address register for RAM
-0008 F8         LDI    .. D = 
-0009 FF                ..   0xFF
-000A A2         PLO 2  .. R2.lo = D
-000B F8         LDI    .. D =
-000C 7F                ..   0x7F Starts from 32k, use FF for 64k RAM
-000D B2         PHI 2  .. R2.hi = D, (R2 == 0x7FFF now)
-000E E2 LOOPHI: SEX 2  .. prepare to output R2.hi
-000F 92         GHI 2  .. D = R2.hi
-000F 64         OUT 4  .. leds = *R2, FIXME: R2
+0003 B2         PHI 2  .. R2.hi = D
+0004 F8         LDI    .. D = 
+0005 xx                ..   0x10, FIXME: END+1
+0006 A1         PLO 1  .. R1.lo = D
+0007 F8         LDI    .. D = 
+0008 xx                ..   0x11, FIXME: END+2
+0009 A2         PLO 2  .. R2.lo = D
+000A E2         SEX 2  .. prepare to output R2.hi (the loop has this too)
+000B 92 LOOP:   GHI 2  .. D = R2.hi
+000C 52         STR 2  .. *R2 = D
+000D 64         OUT 4  .. leds = *R2 (== R2.hi, or gibberish, if RAM fails)
+000E E1         SEX 1  .. X = 1, use R1 as the address register for I/O
+000F 6C         INP 4  .. *R1 = D = switches
+0010 E2         SEX 2  .. X = 2, use R2 as the address register for RAM	
+0011 52         STR 2  .. *R2 = D
+0012 F3         XOR    .. D = xor(D,*R2), which is 0x00, if RAM works!
+0013 3A         BNZ    .. Branch if not 0x00
+0014 xx                ..   to FAIL
 
-.. TODO: D = *R1
-	
-0000 52         STR    .. *R2 = D
-0000 F3         XOR    .. D = xor(D,*R2), which is 0x00, if RAM works!
-0000 3A         BNZ    .. Branch if not 0x00
-0000 xx                ..   to FAIL
+0000 82         GLO 2  .. D = R2.lo
+0000 FB         XRI    .. D = xor(D,
+0000 FF                ..   0xFF) to test if R2.lo != 0xFF
+0013 3A         BNZ    .. Branch if not 0x00
+0000 xx                ..   to NEXT
 
-.. TODO: increment/decrement R2, if still bytes left
-	
+0000 92         GHI 2  .. D = R2.hi
+0000 FB         XRI    .. D = xor(D,
+0000 7F                ..   0x7F) to test if R2.hi != 0x7F (32k limit!)
+0013 32         BZ    .. Branch if 0x00
+0000 xx                ..   to PASS, TODO: or skip next two bytes!
+
+0000 12 NEXT:   INC 2  .. Increment address R2, TODO: do this above
+0000 30         BR     .. Goto
+0000 0B                ..   LOOP	
 0000 38 PASS:   SKP    .. skip error indication
 0000 7B FAIL:   SEQ    .. turn Q on
-000F 00 END:    IDL    .. stop here
+0000 00 END:    IDL    .. stop here
