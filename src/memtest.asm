a8d54f9d9bb4259a8f0168a318dafb3e4301d9ae
Janne Toivola <jammu81@iki.fi>
2015-10-12 15:27:11 +0300

    Bug found: OUT increments the pointer too

--- src/memtest.asm
+++ src/memtest.asm
@@ -6,33 +6,32 @@
 0001 B1         PHI 1  .. R1.hi = D == 0
 0002 B2         PHI 2  .. R2.hi = D == 0
 0003 F8         LDI    .. D = 
-0004 22                ..   0x22 == END+1
+0004 20                ..   0x20 == END+1
 0005 A1         PLO 1  .. R1.lo = D, R1 == END+1
 0006 A2         PLO 2  .. R2.lo = D, R2 == END+1 (but incremented soon)
 0007 7A         REQ    .. Turn Q off to be sure
 0008 E2         SEX 2  .. prepare to OUTput R2.hi (the loop has this too)
-0009 12 LOOP:   INC 2  .. R2++ to test the next byte of RAM
-000A 92         GHI 2  .. D = R2.hi (value to be displayed)
-000B 52         STR 2  .. *R2 = D   (since OUT uses only stuff in RAM)
-000C 64         OUT 4  .. leds = *R2 (== R2.hi, or gibberish, if RAM fails?)
-000D E1         SEX 1  .. X = 1, use R1 as the address register for input
-000E 6C         INP 4  .. *R1 = D = input switches
-000F E2         SEX 2  .. X = 2, use R2 as the address register for testing	
-0010 52         STR 2  .. *R2 = D
-0011 F3         XOR    .. D = xor(D,*R2), which is 0x00, if RAM works!
-0012 3A         BNZ    .. Branch if not 0x00
-0013 20                ..   to FAIL
-0014 82         GLO 2  .. D = R2.lo
-0015 FB         XRI    .. D = xor(D,
-0016 FF                ..   0xFF) to test if R2.lo != 0xFF
-0017 3A         BNZ    .. Branch if not 0x00 (at least few bytes left)
-0018 09                ..   to LOOP
-0019 92         GHI 2  .. D = R2.hi (R2.lo == 0xFF below this point)
-001A FB         XRI    .. D = xor(D,
-001B 7F                ..   0x7F) to test if R2.hi != 0x7F (32k limit!)
-001C 3A         BNZ    .. Branch
-001D 09                ..   to LOOP (more than 256 bytes left)
-001E 7A PASS:   REQ    .. Q off (redundant)
-001F 00         IDL    .. stop, leaving R2.hi visible, but Q off
-0020 7B FAIL:   SEQ    .. turn Q on (TODO: blink R2.lo / R2.hi alternately)
-0021 00         IDL    .. stop, leaving R2.hi and Q visible
+0009 92 LOOP:   GHI 2  .. D = R2.hi (value to be displayed)
+000A 52         STR 2  .. *R2 = D   (since OUT uses only stuff in RAM)
+000B 64         OUT 4  .. leds = *(R2++) (R2.hi, or gibberish, if RAM fails?)
+000C E1         SEX 1  .. X = 1, use R1 as the address register for input
+000D 6C         INP 4  .. *R1 = D = input switches
+000E E2         SEX 2  .. X = 2, use R2 as the address register for testing	
+000F 52         STR 2  .. *R2 = D
+0010 F3         XOR    .. D = xor(D,*R2), which is 0x00, if RAM works!
+0011 3A         BNZ    .. Branch if not 0x00
+0012 20                ..   to FAIL
+0013 82         GLO 2  .. D = R2.lo
+0014 FB         XRI    .. D = xor(D,
+0015 FF                ..   0xFF) to test if R2.lo != 0xFF
+0016 3A         BNZ    .. Branch if not 0x00 (at least few bytes left)
+0017 09                ..   to LOOP
+0018 92         GHI 2  .. D = R2.hi (R2.lo == 0xFF below this point)
+0019 FB         XRI    .. D = xor(D,
+001A 7F                ..   0x7F) to test if R2.hi != 0x7F (32k limit!)
+001B 3A         BNZ    .. Branch
+001C 09                ..   to LOOP (more than 256 bytes left)
+001D 00 PASS:   IDL    .. stop, leaving R2.hi visible, but Q off
+001E 7B FAIL:   SEQ    .. turn Q on (TODO: blink R2.lo / R2.hi alternately)
+001F 00         IDL    .. stop, leaving R2.hi and Q visible
+0020 00                .. for storing the test pattern (*R1)
