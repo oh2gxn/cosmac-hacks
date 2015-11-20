a847472cce788f0084189f1671e83ea57364d759
Janne Toivola <jammu81@iki.fi>
2015-11-20 22:20:39 +0200

    Longer delay for memdump. Realized that R1 and R2 are used by ISR, so using R3-RF.

--- src/memdump.asm
+++ src/memdump.asm
@@ -1,25 +1,28 @@
 .. Dumps each byte of RAM to output (TODO: control the address and loop over?)
 .. Author: Janne Toivola
 0000 90 INIT:   GHI 0  .. D = R0.hi == 0 (since PC is reset to INIT)
-0001 B1         PHI 1  .. R1.hi = D == 0
-0002 A1         PLO 1  .. R1.lo = D, R1 == 0
-0003 E1         SEX 1  .. to use *R1 in OUT
-0004 64 LOOP:   OUT 4  .. leds = *(R1++)
-0005 90         GHI 0  .. D = R0.hi == 0
-0006 FC DELAY:  ADI    .. D += 
-0007 01                ..   1
-0008 3B         BNF    .. Branch if DF==0 (D < 256)
-0009 06                ..   to DELAY
-000A 81         GLO 1  .. D = R1.lo
-000B FB         XRI    .. D = xor(D,
-000C FF                ..   0xFF) to test if R1.lo != 0xFF
-000D 3A         BNZ    .. Branch
-000E 04                ..   to LOOP (at least few bytes left)
-000F 91         GHI 1  .. D = R1.hi (R1.lo == 0xFF at this point)
-0010 FB         XRI    .. D = xor(D,
-0011 FF                ..   0xFF) to test R1.hi (Use 7F for 32k RAM!)
-0012 3A         BNZ    .. Branch
-0013 04                ..   to LOOP (at least 256 bytes left)
-0014 7B         SEQ    .. turn Q on
-0015 00 STOP:   IDL    .. stop, leaving Q on
-0016 00 DATA:          .. (this area of the memory was to be explored)
+0001 B3         PHI 3  .. R3.hi = D == 0
+0002 F8         LDI    .. D = 
+0003 19                ..   DATA
+0004 A3         PLO 3  .. R3.lo = D, R3 == DATA (skip this program)
+0005 E3         SEX 3  .. to use *R3 in OUT
+0006 64 LOOP:   OUT 4  .. leds = *(R3++)
+0007 90         GHI 0  .. D = R0.hi == 0
+0008 C4 DELAY:  NOP    .. do nothing for a while (adjusts the delay time)
+0009 FC         ADI    .. D += 
+000A 01                ..   1   (speed of doing nothing for a while)
+000B 3B         BNF    .. Branch if DF==0 (D < 256)
+000C 06                ..   to DELAY
+000D 83         GLO 3  .. D = R3.lo
+000E FB         XRI    .. D = xor(D,
+000F FF                ..   0xFF) to test if R3.lo != 0xFF
+0010 3A         BNZ    .. Branch
+0011 04                ..   to LOOP (at least few bytes left)
+0012 93         GHI 3  .. D = R3.hi (R3.lo == 0xFF at this point)
+0013 FB         XRI    .. D = xor(D,
+0014 FF                ..   0xFF) to test R3.hi (Use 7F for 32k RAM!)
+0015 3A         BNZ    .. Branch
+0016 04                ..   to LOOP (at least 256 bytes left)
+0017 7B         SEQ    .. turn Q on
+0018 00 STOP:   IDL    .. stop, leaving Q on
+0019 00 DATA:          .. (this area of the memory was to be explored)
--- src/memtest.asm
+++ src/memtest.asm
@@ -8,42 +8,42 @@
 .. then reading and testing it didn't get "out of phase" at some boundary.
 .. Author: Janne Toivola
 0000 90 INIT:   GHI 0  .. D = R0.hi == 0 (since PC is reset to INIT)
-0001 B1         PHI 1  .. R1.hi = D == 0
-0002 B2         PHI 2  .. R2.hi = D == 0
-0003 B3         PHI 3  .. R3.hi = D == 0
+0001 B3         PHI 3  .. R3.hi = D == 0
+0002 B4         PHI 4  .. R4.hi = D == 0
+0003 B5         PHI 5  .. R5.hi = D == 0
 0004 F8         LDI    .. D = 
 0005 26                ..   DATA address
-0006 A1         PLO 1  .. R1.lo = D, R1 == DATA (almost fixed)
-0007 A2         PLO 2  .. R2.lo = D, R2 == DATA (but incremented soon)
-0008 A3         PLO 3  .. R2.lo = D, R3 == DATA (but incremented soon)
-0009 12         INC 2  .. R2++, DATA+1 is the address for test pattern
-000A E2         SEX 2  .. X=2, R2 is the address register for input
-000B 6C         INP 4  .. *R2 = D = input switches
-000C E3         SEX 3  .. X=3, R3 is the address register for testing
-000D 60 LOOP:   IRX    .. R3++, next address to be tested (== R2 at first)
-000E 93         GHI 3  .. D = R3.hi (value to be displayed)
-000F 51         STR 1  .. *R1 = D   (since OUT uses only RAM, not registers)
-0010 E1         SEX 1  .. to use *R1 in OUT
-0011 64         OUT 4  .. leds = *(R1++) (high byte of the current address)
-0012 21         DEC 1  .. R1--, to compensate the damn address increment
-0013 E3         SEX 3  .. back to using R3 (in XOR and IRX)
-0014 02         LDN 2  .. D = *R2
-0015 53         STR 3  .. *R3 = D
-0016 F3         XOR    .. D = xor(D,*R3), which is 0x00, if RAM works!
+0006 A3         PLO 3  .. R3.lo = D, R3 == DATA (almost fixed)
+0007 A4         PLO 4  .. R4.lo = D, R4 == DATA (but incremented soon)
+0008 A5         PLO 5  .. R5.lo = D, R5 == DATA (but incremented soon)
+0009 14         INC 4  .. R4++, DATA+1 is the address for test pattern
+000A E4         SEX 4  .. X=4, R4 is the address register for input
+000B 6C         INP 4  .. *R4 = D = input switches
+000C E5         SEX 5  .. X=5, R5 is the address register for testing
+000D 60 LOOP:   IRX    .. R5++, next address to be tested (== R4 at first)
+000E 95         GHI 5  .. D = R5.hi (value to be displayed)
+000F 53         STR 3  .. *R3 = D   (since OUT uses only RAM, not registers)
+0010 E3         SEX 3  .. to use *R3 in OUT
+0011 64         OUT 4  .. leds = *(R3++) (high byte of the current address)
+0012 23         DEC 3  .. R3--, to compensate the damn address increment
+0015 E5         SEX 5  .. back to using R5 (in XOR and IRX)
+0014 04         LDN 4  .. D = *R4
+0015 55         STR 5  .. *R5 = D
+0016 F3         XOR    .. D = xor(D,*R5), which is 0x00, if RAM works!
 0017 3A         BNZ    .. Branch
 0018 24                ..   to FAIL
-0019 83         GLO 3  .. D = R3.lo
+0019 85         GLO 5  .. D = R5.lo
 001A FB         XRI    .. D = xor(D,
-001B FF                ..   0xFF) to test if R3.lo != 0xFF
+001B FF                ..   0xFF) to test if R5.lo != 0xFF
 001C 3A         BNZ    .. Branch
 001D 0D                ..   to LOOP (at least few bytes left)
-001E 93         GHI 3  .. D = R3.hi (R3.lo == 0xFF at this point)
+001E 95         GHI 5  .. D = R5.hi (R5.lo == 0xFF at this point)
 001F FB         XRI    .. D = xor(D,
-0020 FF                ..   0xFF) to test R3.hi (Use 7F for 32k RAM!)
+0020 FF                ..   0xFF) to test R5.hi (Use 7F for 32k RAM!)
 0021 3A         BNZ    .. Branch
 0022 0D                ..   to LOOP (at least 256 bytes left)
-0023 00 PASS:   IDL    .. stop, leaving R3.hi visible, but Q off
-0024 7B FAIL:   SEQ    .. turn Q on (TODO: blink R3.lo / R3.hi / *R3 ?)
-0025 00         IDL    .. stop, leaving R3.hi visible and Q ON
-0026 00 DATA:          .. for storing the displayed value *R1
-0027 00                .. for storing the test pattern *R2
+0023 00 PASS:   IDL    .. stop, leaving R5.hi visible, but Q off
+0024 7B FAIL:   SEQ    .. turn Q on (TODO: blink R5.lo / R5.hi / *R5 ?)
+0025 00         IDL    .. stop, leaving R5.hi visible and Q ON
+0026 00 DATA:          .. for storing the displayed value *R3
+0027 00                .. for storing the test pattern *R4
